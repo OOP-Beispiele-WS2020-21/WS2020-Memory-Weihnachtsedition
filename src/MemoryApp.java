cbf4a333d1eea5eaead50d57743b08c3d2e8b2ec
Alexander Bazo <alexanderbazo@googlemail.com>
2020-12-22 13:24:00 +0100

    Rework GamConfig to abstract class

--- src/MemoryApp.java
+++ src/MemoryApp.java
@@ -2,17 +2,17 @@ import config.GameConfig;
 import de.ur.mi.oop.app.GraphicsApp;
 import de.ur.mi.oop.launcher.GraphicsAppLauncher;
 
-public class MemoryApp extends GraphicsApp implements GameConfig {
+public class MemoryApp extends GraphicsApp {
 
     @Override
     public void initialize() {
         setFrameRate(30);
-        setCanvasSize(WINDOW_WIDTH, WINDOW_HEIGHT);
+        setCanvasSize(GameConfig.WINDOW_WIDTH, GameConfig.WINDOW_HEIGHT);
     }
 
     @Override
     public void draw() {
-        drawBackground(BACKGROUND_COLOR);
+        drawBackground(GameConfig.BACKGROUND_COLOR);
     }
 
     public static void main(String[] args) {
